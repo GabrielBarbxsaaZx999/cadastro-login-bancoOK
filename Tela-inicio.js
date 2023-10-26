c762cbc24611d8ff7424a873c3055ac87f080fb4
Calorithefacto <125594287+VictorCalori@users.noreply.github.com>
2023-10-26 10:52:57 -0300

    Add files via upload

---
+++ Tela-inicio.js
@@ -0,0 +1,26 @@
+function redirectCadastrese() {
+    window.location="TeladeCadastro.html";}
+
+function redirectLogin(){
+    window.location="Login.html";
+}
+
+function redirectQuemsomos(){
+    window.location="quemsomos.html";
+}
+
+function redirectAgendar(){
+    window.location="consulta.html"
+}
+
+function redirectAjuda(){
+    window.location.assign("https://www.google.com")
+}
+
+function redirectOnde(){
+    window.location="Localização.html"
+}
+
+function redirectOutros(){
+    window.location.assign("https://www.google.com")
+}
